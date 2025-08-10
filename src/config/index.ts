42d2b648c6eb60c6bd6a86dd7c46b689603679ef
DucTai <69153374+taicutm@users.noreply.github.com>
2025-08-10 23:46:48 +0700

    Xóa hết file giữ lại README.md

--- src/config/index.ts
+++
@@ -1,6 +0,0 @@
-// import { monadTestnet } from '@reown/appkit/networks'
-
-// export const networks = [monadTestnet]
-import { monadTestnet, AppKitNetwork } from '@reown/appkit/networks'
-
-export const networks = [monadTestnet] as [AppKitNetwork, ...AppKitNetwork[]]
